65375ab0ab564f5072a5d31698bdf75b7f2ca036
Baekfour <102018770+Baekfour@users.noreply.github.com>
2022-04-22 08:04:41 +0900

    Add files via upload

--- src/project/OrderData.java
+++ src/project/OrderData.java
@@ -2,25 +2,147 @@ package project;
 
 public class OrderData {
 
-		public int inputTicketType = 0;
-		public int inputTicketTime = 0;
-		public int inputRegisterNum = 0;
-		public int inputRegisterNum2 = 0;
-		public int inputbenefit = 0;
-		public int inputbasicFeeType = 0;
-		public int price = 0;
-		public int money = 0;
-		public int extend = 0;
-		public int totalmoney = 0;
-		public int numberofticket = 0;
-		public int basicFeeType = 0;
-		public int orderCount = 0;
-		public int addTicket = 0;
-		public int today = 0;
-		public int year = 0;
-		public int month = 0;
-		public int day = 0;
-		public int RealAge = 0;
-		public int disprice = 0;
+		private int inputTicketType = 0;
+		private int inputTicketTime = 0;
+		private int inputRegisterNum = 0;
+		private int inputRegisterNum2 = 0;
+		private int inputbenefit = 0;
+		private int inputbasicFeeType = 0;
+		private int price = 0;
+		private int money = 0;
+		private int extend = 0;
+		private int totalmoney = 0;
+		private int numberofticket = 0;
+		private int basicFeeType = 0;
+		private int orderCount = 0;
+		private int addTicket = 0;
+		private int today = 0;
+		private int year = 0;
+		private int month = 0;
+		private int day = 0;
+		private int RealAge = 0;
+		private int disprice = 0;
+		
+		
+		public int getInputTicketType() {
+			return inputTicketType;
+		}
+		public void setInputTicketType(int inputTicketType) {
+			this.inputTicketType = inputTicketType;
+		}
+		public int getInputTicketTime() {
+			return inputTicketTime;
+		}
+		public void setInputTicketTime(int inputTicketTime) {
+			this.inputTicketTime = inputTicketTime;
+		}
+		public int getInputRegisterNum() {
+			return inputRegisterNum;
+		}
+		public void setInputRegisterNum(int inputRegisterNum) {
+			this.inputRegisterNum = inputRegisterNum;
+		}
+		public int getInputRegisterNum2() {
+			return inputRegisterNum2;
+		}
+		public void setInputRegisterNum2(int inputRegisterNum2) {
+			this.inputRegisterNum2 = inputRegisterNum2;
+		}
+		public int getInputbenefit() {
+			return inputbenefit;
+		}
+		public void setInputbenefit(int inputbenefit) {
+			this.inputbenefit = inputbenefit;
+		}
+		public int getInputbasicFeeType() {
+			return inputbasicFeeType;
+		}
+		public void setInputbasicFeeType(int inputbasicFeeType) {
+			this.inputbasicFeeType = inputbasicFeeType;
+		}
+		public int getPrice() {
+			return price;
+		}
+		public void setPrice(int price) {
+			this.price = price;
+		}
+		public int getMoney() {
+			return money;
+		}
+		public void setMoney(int money) {
+			this.money = money;
+		}
+		public int getExtend() {
+			return extend;
+		}
+		public void setExtend(int extend) {
+			this.extend = extend;
+		}
+		public int getTotalmoney() {
+			return totalmoney;
+		}
+		public void setTotalmoney(int totalmoney) {
+			this.totalmoney = totalmoney;
+		}
+		public int getNumberofticket() {
+			return numberofticket;
+		}
+		public void setNumberofticket(int numberofticket) {
+			this.numberofticket = numberofticket;
+		}
+		public int getBasicFeeType() {
+			return basicFeeType;
+		}
+		public void setBasicFeeType(int basicFeeType) {
+			this.basicFeeType = basicFeeType;
+		}
+		public int getOrderCount() {
+			return orderCount;
+		}
+		public void setOrderCount(int orderCount) {
+			this.orderCount = orderCount;
+		}
+		public int getAddTicket() {
+			return addTicket;
+		}
+		public void setAddTicket(int addTicket) {
+			this.addTicket = addTicket;
+		}
+		public int getToday() {
+			return today;
+		}
+		public void setToday(int today) {
+			this.today = today;
+		}
+		public int getYear() {
+			return year;
+		}
+		public void setYear(int year) {
+			this.year = year;
+		}
+		public int getMonth() {
+			return month;
+		}
+		public void setMonth(int month) {
+			this.month = month;
+		}
+		public int getDay() {
+			return day;
+		}
+		public void setDay(int day) {
+			this.day = day;
+		}
+		public int getRealAge() {
+			return RealAge;
+		}
+		public void setRealAge(int realAge) {
+			RealAge = realAge;
+		}
+		public int getDisprice() {
+			return disprice;
+		}
+		public void setDisprice(int disprice) {
+			this.disprice = disprice;
+		}
 		
 	}
