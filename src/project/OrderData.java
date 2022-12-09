7418121cb2b524c8f05c82e232529c86e03dcf35
Baekfour <102018770+Baekfour@users.noreply.github.com>
2022-12-09 10:07:41 +0900

    수정

--- src/project/OrderData.java
+++ src/project/OrderData.java
@@ -3,6 +3,7 @@ package project;
 public class OrderData {
 
 		// commit해볼께
+		// github 웹에서 수정 후 커밋
 		private int inputTicketType = 0;
 		private int inputTicketTime = 0;
 		private int inputRegisterNum = 0;
